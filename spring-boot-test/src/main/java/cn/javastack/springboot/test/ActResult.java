9eded1fe03228ef3af56509598832152f58a7c12
740202157@qq.com <740202157@qq.com>
2021-01-13 20:23:52 +0800

    setter and getter

--- spring-boot-test/src/main/java/cn/javastack/springboot/test/ActResult.java
+++ spring-boot-test/src/main/java/cn/javastack/springboot/test/ActResult.java
@@ -1,13 +1,34 @@
 package cn.javastack.springboot.test;
 
-import lombok.Data;
 
 
-@Data
 public class ActResult {
 
     private int code = 0;
     private String msg;
     private Object data;
 
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
 }
